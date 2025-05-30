31c08a30b96612ddb23aa44f3c33f2aeadf213cf
rakib <rakib.hasan.cse.pust@gmail.com>
2025-05-30 15:03:12 +0600

    first

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+Product.delete_all
+
+Product.create!(
+  title: "Design and Build Great Web APIs",
+  description: %{
+    Docker is an open platform for developing, shipping, and running applications. Docker enables you to separate your applications from your infrastructure so you can deliver software quickly. With Docker, you can manage your infrastructure in the same ways you manage your applications. By taking advantage of Docker's methodologies for shipping, testing, and deploying code, you can significantly reduce the delay between writing code and running it in production.
+  },
+  image_url: '2025.jpg',
+  price: 24.95
+)
